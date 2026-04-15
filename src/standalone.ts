66b7bba11dd7bc4238477a9047949cd679faeb20
itsrealfortune <176779748+itsrealfortune@users.noreply.github.com>
2026-04-15 11:42:48 +0200

    feat: initialize SSH Mimic shell and add demo command

--- src/standalone.ts
+++ src/standalone.ts
@@ -6,7 +6,16 @@ const sshMimic = new VirtualMachine({ port: 2222, hostname: sshHostname });
 sshMimic
 	.start()
 	.then((port: number) => {
-		console.log(`SSH Mimic initialized. Listening on port ${port}.`);
+		if (!sshMimic.shell) console.error("Failed to initialize SSH Mimic shell.");
+		else {
+			console.log(`SSH Mimic initialized. Listening on port ${port}.`);
+			sshMimic.shell.addCommand("demo", [], () => {
+				return {
+					stdout: "This is a demo command. It does nothing useful.",
+					exitCode: 0,
+				};
+			});
+		}
 	})
 	.catch((error: unknown) => {
 		console.error("Failed to start SSH Mimic:", error);
