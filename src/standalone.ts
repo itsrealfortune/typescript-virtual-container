d078d3021f8a8ab2d0f0fccd2d2973432370b1c3
itsrealfortune <176779748+itsrealfortune@users.noreply.github.com>
2026-04-17 09:39:15 +0200

    feat: anti-crashing

--- src/standalone.ts
+++ src/standalone.ts
@@ -36,3 +36,15 @@ new VirtualSftpServer({ port: 2223, hostname, shell: virtualShell })
 		console.error("Failed to start SFTP Mimic:", error);
 		process.exit(1);
 	});
+
+process.on("uncaughtException", (error) => {
+	console.log("Oh my god, something terrible happened: ", error);
+});
+
+process.on("unhandledRejection", (error, promise) => {
+	console.log(
+		" Oh Lord! We forgot to handle a promise rejection here: ",
+		promise,
+	);
+	console.log(" The error was: ", error);
+});
