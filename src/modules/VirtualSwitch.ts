0e162ab3d9f872eed4930fbda0a26554ae0aab8d
itsrealfortune <176779748+itsrealfortune@users.noreply.github.com>
2026-05-20 15:37:53 +0200

    fix: correct access to arpCache in VirtualSwitch class

--- src/modules/VirtualSwitch.ts
+++ src/modules/VirtualSwitch.ts
@@ -95,7 +95,7 @@ export class VirtualSwitch {
 		const port: VmPort = { mac, ip, shell };
 		this.ipToMac.set(ip, mac);
 		// Register in ARP cache
-		this.network["arpCache"].push({ ip, mac, device: "eth0", state: "REACHABLE" } as never);
+		this.network.arpCache.push({ ip, mac, device: "eth0", state: "REACHABLE" } as never);
 		return port;
 	}
 
